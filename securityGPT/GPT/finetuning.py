c9d84c2982a1ab05992e80bfe2b275ec56d9df5d
alexyskoutnev <alexyskoutnev@gmail.com>
2023-12-06 23:11:01 -0600

    update

--- securityGPT/GPT/finetuning.py
+++ securityGPT/GPT/finetuning.py
@@ -31,18 +31,19 @@ def plot_loss(data : dict):
     for key in data.keys():
         epochs = np.arange(len(data[key]))
         plt.plot(epochs, data[key], label='Line Graph')
-        plt.xlabel('Epochs Loss')
+        plt.xlabel('Epochs')
         plt.ylabel('Loss')
-        plt.savefig(path + "_" + str(key) + "_gpt_" + ".png")
-
-def plot_acc(data : dict):
-    path = os.path.join(dataset_output)
-    for key in data.keys():
-        epochs = np.arange(len(data[key]))
-        plt.plot(epochs, data[key], label='Line Graph')
-        plt.xlabel('Epochs Loss')
-        plt.ylabel('Accurancy')
         plt.savefig(path + str(key) + "_gpt_" + ".png")
+        plt.close()
+
+def plot_acc(data : dict, plot_type="testing_acc"):
+    path = os.path.join(dataset_output)
+    epochs = np.arange(len(data[plot_type]))
+    plt.plot(epochs, data[plot_type], label='Line Graph')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accurancy (%)')
+    plt.savefig(path + str(plot_type) + "_gpt_" + ".png")
+    plt.close()
 
 def train(model, dataloader : DataLoader, optimizer_ : torch.optim, scheduler_, device_):
   r"""
@@ -162,78 +163,3 @@ def validation(model, dataloader : DataLoader, device : dict):
             predictions_labels += predict_content
     avg_epoch_loss = total_loss / len(dataloader)
     return true_labels, predictions_labels, avg_epoch_loss
-
-if __name__ == "__main__":
-    #============== Config =============
-    set_seed(123)
-    epochs = 10
-    batch_size = 10
-    max_length = 256
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model_name_or_path = 'gpt2'
-    labels_ids = {'0': 0, '1': 1}
-    n_labels = len(labels_ids)
-    #============== Config ===============
-    #============== Loading Model ========
-    print('Loading configuraiton...')
-    model_config = GPT2Config.from_pretrained(pretrained_model_name_or_path=model_name_or_path, num_labels=n_labels)
-    tokenizer = GPT2Tokenizer.from_pretrained(pretrained_model_name_or_path=model_name_or_path)
-    tokenizer.padding_side = "left"
-    tokenizer.pad_token = tokenizer.eos_token
-    model = GPT2ForSequenceClassification.from_pretrained(pretrained_model_name_or_path=model_name_or_path, config=model_config)
-    model.resize_token_embeddings(len(tokenizer))
-    model.config.pad_token_id = model.config.eos_token_id
-    model.to(device)
-    print('Model loaded to `%s`'%device)
-    #============== Setup =================
-    gpt2_classificaiton_collator = Gpt2ClassificationCollator(use_tokenizer=tokenizer, 
-                                                          labels_encoder=labels_ids, 
-                                                          max_sequence_len=max_length)
-    dataset_path = os.path.join("../../data")
-    size = 100
-    #============== Loading Dataset ============== 
-    train_dataset = DataTorch(dataset_path, size=size, use_tokenizer=None,
-                              bootstrap=True, ratio=0.2, train_data=True)
-    train_dataloader = DataLoader(train_dataset, batch_size=batch_size,
-                                  shuffle=True, collate_fn=gpt2_classificaiton_collator)
-    test_dataset = DataTorch(dataset_path, size=size, use_tokenizer=None,
-                              bootstrap=True, ratio=0.2, test_data=True)
-    test_dataloader = DataLoader(test_dataset, batch_size=batch_size,
-                                  shuffle=True, collate_fn=gpt2_classificaiton_collator)
-    #============== Loading Dataset ============== 
-    print('Created `train_dataloader` with %d batches!'%len(train_dataloader))
-    optimizer = AdamW(model.parameters(),
-                  lr = 2e-5, # default is 5e-5
-                  eps = 1e-8 # default is 1e-8.
-                  )
-    total_steps = len(train_dataloader) * epochs
-    scheduler = get_linear_schedule_with_warmup(optimizer, 
-                                            num_warmup_steps = 0, # Default value in run_glue.py
-                                            num_training_steps = total_steps)
-    all_loss = {'train_loss':[], 'testing_loss':[]}
-    all_acc = {'train_acc':[], 'testing_acc':[]}
-    all_f1 = {"train_f1" : [], "testing_f1" : []}
-    #=============== Training Loop =========
-    for epoch in range(epochs):
-        # Training Eval
-        train_labels, train_predict, train_loss = train(model, train_dataloader, optimizer, scheduler, device)
-        train_acc = accuracy_score(train_labels, train_predict)
-        # Testing Eval
-        testing_labels, testing_predict, testing_loss = validation(model, test_dataloader, device)
-        testing_acc = accuracy_score(testing_labels, testing_predict)
-        print("train_loss: %.5f - train_acc: %.5f | testing_loss: %.5f - testing_acc %.5f"%(train_loss, train_acc, testing_loss,  testing_acc))
-        f1_testing = f1_score(testing_labels, testing_predict)
-        f1_training = f1_score(train_labels, train_predict)
-        print("training f1: %.5f  -  testing f1: %.5f" % (f1_training, f1_testing))
-        #================== Logging ================== 
-        all_loss['train_loss'].append(train_loss)
-        all_acc['train_acc'].append(train_loss)
-        all_loss['testing_loss'].append(testing_loss)
-        all_acc['testing_acc'].append(testing_acc)
-        all_f1['train_f1'].append(f1_training)
-        all_f1['testing_f1'].append(f1_testing)
-        #================== Logging ================== 
-    #=============== Training Loop =========    
-    print("GPT has been finetuned")
-    plot_loss(all_loss)
-    plot_acc(all_acc)
